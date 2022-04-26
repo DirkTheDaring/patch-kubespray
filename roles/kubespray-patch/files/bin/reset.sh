adb7ca71e2eec56e7da832fc88465dd925234c42
Dietmar Kling <dietmar.kling@googlemail.com>
2022-04-26 12:44:30 +0200

    initial

---
+++ roles/kubespray-patch/files/bin/reset.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+set -eux
+# always call this symlinked
+# kubespray-<name>-<stage>-<variant>.sh
+# This always assumes that the script will run in base dir of kubespray
+# the name,stage and variant will be mapped to the variables
+
+function pick
+{
+    for PATHNAME in "$@"; do
+        if [ -e "$PATHNAME" ]; then
+            echo $PATHNAME
+            return
+        fi
+    done
+}
+
+BASENAME=$(basename -s .sh "$0")
+DIRNAME=$(dirname "$0")
+
+# Split basename into array
+ARRAY=(${BASENAME//-/ })
+
+CONFIG_NAME=${ARRAY[1]}
+CONFIG_STAGE=${ARRAY[2]}
+CONFIG_VARIANT=${ARRAY[3]}
+
+ALL_FILE=$(pick\
+ "inventory/clusters/$CONFIG_NAME/$CONFIG_STAGE/all.yaml"\
+ "inventory/clusters/$CONFIG_NAME/all.yaml")
+
+[ -n "${ALL_FILE}" ] && OPTION_ALL_FILE="-i $ALL_FILE"
+
+INVENTORY_FILE=$(pick "inventory/clusters/${CONFIG_NAME}/${CONFIG_STAGE}/inventory.yaml")
+ INVENTORY_DIR=$(pick "inventory/${CONFIG_VARIANT}")
+
+[ -z "${INVENTORY_DIR}" ] && echo "inventory dir does not exist: inventory/${CONFIG_NAME}/${CONFIG_STAGE}" && exit 1
+
+ansible-playbook\
+    $OPTION_ALL_FILE\
+    -i "$INVENTORY_FILE"\
+    -i "$INVENTORY_DIR"\
+    reset.yml\
+        --flush-cache\
+        -b \
+        "$@"
